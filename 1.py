c0d7c360f5a1c580a9819e1896bb42b9aff44331
Joshua Equere <marveloper.8@gmail.com>
2022-01-16 21:12:34 +0100

    create table with name and address column

--- 1.py
+++ 1.py
@@ -3,13 +3,15 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='password'
+    password='password',
     database='mydatabase'
 )
 
 mycursor = mydb.cursor()
-# mycursor.execute("CREATE DATABASE mydatabase")
-mycursor.execute("SHOW DATABASES")
 
-for x in mycursor:
-    print(x)
+mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+# mycursor.execute("CREATE DATABASE mydatabase")
+# mycursor.execute("SHOW DATABASES")
+
+# for x in mycursor:
+#     print(x)
