197c619687391bb4473b436a8130ab63887d3910
Joshua Equere <marveloper.8@gmail.com>
2022-01-16 21:53:09 +0100

    add primary key

--- 1.py
+++ 1.py
@@ -10,9 +10,10 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 # mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
-mycursor.execute("SHOW TABLES")
-for x in mycursor:
-    print(x)
+# mycursor.execute("SHOW TABLES")
+# for x in mycursor:
+#     print(x)
+mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
 
 # mycursor.execute("CREATE DATABASE mydatabase")
 # mycursor.execute("SHOW DATABASES")
