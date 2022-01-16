061dd626c6044225a9dd9baddf36852454f03553
Joshua Equere <marveloper.8@gmail.com>
2022-01-16 20:37:36 +0100

    get list of databases

--- 1.py
+++ 1.py
@@ -7,4 +7,8 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE mydatabase")
+# mycursor.execute("CREATE DATABASE mydatabase")
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+    print(x)
