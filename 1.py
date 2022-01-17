fa6cfe98ea996dcce34f6e90e53c8f4a453aadb8
Joshua Equere <marveloper.8@gmail.com>
2022-01-17 05:47:14 +0100

    insert one row and return the id

--- 1.py
+++ 1.py
@@ -10,25 +10,32 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 sql = 'INSERT INTO customers (name, address) VALUES (%s, %s)'
-val = [
-  ('Peter', 'Lowstreet 4'),
-  ('Amy', 'Apple st 652'),
-  ('Hannah', 'Mountain 21'),
-  ('Michael', 'Valley 345'),
-  ('Sandy', 'Ocean blvd 2'),
-  ('Betty', 'Green Grass 1'),
-  ('Richard', 'Sky st 331'),
-  ('Susan', 'One way 98'),
-  ('Vicky', 'Yellow Garden 2'),
-  ('Ben', 'Park Lane 38'),
-  ('William', 'Central st 954'),
-  ('Chuck', 'Main Road 989'),
-  ('Viola', 'Sideway 1633')
-]
-mycursor.executemany(sql, val)
+val = ('Michelle', 'Blue Village')
+mycursor.execute(sql, val)
 
 mydb.commit()
-print(mycursor.rowcount, "record inserted.")
+print('1 record inserted, ID:', mycursor.lastrowid)
+
+# sql = 'INSERT INTO customers (name, address) VALUES (%s, %s)'
+# val = [
+#   ('Peter', 'Lowstreet 4'),
+#   ('Amy', 'Apple st 652'),
+#   ('Hannah', 'Mountain 21'),
+#   ('Michael', 'Valley 345'),
+#   ('Sandy', 'Ocean blvd 2'),
+#   ('Betty', 'Green Grass 1'),
+#   ('Richard', 'Sky st 331'),
+#   ('Susan', 'One way 98'),
+#   ('Vicky', 'Yellow Garden 2'),
+#   ('Ben', 'Park Lane 38'),
+#   ('William', 'Central st 954'),
+#   ('Chuck', 'Main Road 989'),
+#   ('Viola', 'Sideway 1633')
+# ]
+# mycursor.executemany(sql, val)
+
+# mydb.commit()
+# print(mycursor.rowcount, "record inserted.")
 
 # mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
 # mycursor.execute("SHOW TABLES")
